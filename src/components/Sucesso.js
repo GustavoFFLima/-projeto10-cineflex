e085d1628a6ccb14a4bbdc9617854851f2bc63e7
GustavoFFLima <frazao39@gmail.com>
2022-12-12 23:11:47 -0300

    estrutura para estilização final

--- src/components/Sucesso.js
+++ src/components/Sucesso.js
@@ -10,7 +10,28 @@ export default function Sucesso( {arrayDados, setArrayDados } ) {
     }
     
     return  (
-        console.log(arrayDados)
+        <SucessoStyled>
+            <div>
+                <h1>Pedido feito</h1>
+                <h1>com sucesso!</h1> 
+            </div>
+            <div>
+                <h2>Filme e sessão</h2>
+                <p>Enola Holmes</p>
+                <p>24/06/2021 15:00</p>
+            </div>
+            <div>
+                <h2>Ingressos</h2>
+                <p>Assento 15</p>
+                <p>Assento 16</p>
+            </div>
+            <div>
+                <h2>Comprador</h2>
+                <p>Nome: João da Silva Sauro</p>
+                <p>CPF: 123.456.789-10</p>
+            </div>
+            <button onClick={home}>Voltar pra Home</button>
+        </SucessoStyled>
         // <SucessoStyled>
         //     <div>
         //         <h1>Pedido feito</h1>
