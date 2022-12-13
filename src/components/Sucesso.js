14f5187e0f47d8615aba2b4577259f794b88689d
GustavoFFLima <frazao39@gmail.com>
2022-12-12 23:10:31 -0300

    tag's finais correção automaticaa

--- src/components/Sucesso.js
+++ src/components/Sucesso.js
@@ -16,21 +16,21 @@ export default function Sucesso( {arrayDados, setArrayDados } ) {
         //         <h1>Pedido feito</h1>
         //         <h1>com sucesso!</h1> 
         //     </div>
-        //     <div>
+        //     <div data-test="movie-info">
         //         <h2>Filme e sessão</h2>
         //         <p>{arrayDados.filme}</p>
         //         <p>{arrayDados.sessao} {arrayDados.horaSessao}</p>
         //     </div>
-        //     <div>
+        //     <div data-test="seats-info">
         //         <h2>Ingressos</h2>
         //         {arrayDados.map((tiket) => <p>Assento {tiket.ingressos}</p> )}
         //     </div>
-        //     <div>
+        //     <div  data-test="client-info">
         //         <h2>Comprador</h2>
         //         <p>Nome: {arrayDados.nome}</p>
         //         <p>CPF: {arrayDados.cpf}</p>
         //     </div>
-        //     <button onClick={home}>Voltar pra Home</button>
+        //     <button data-test="go-home-btn" onClick={home}>Voltar pra Home</button>
         // </SucessoStyled>      
     )
 }
